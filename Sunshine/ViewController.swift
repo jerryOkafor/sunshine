63a2ecdee80cbefbb36734a9acd4660c22b0c348
Jerry Hanks <jerryhanksokafor@gmail.com>
2019-08-18 00:49:59 +0100

    Initial Commit

---
+++ Sunshine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunshine
+//
+//  Created by Jerry Hanks on 18/08/2019.
+//  Copyright © 2019 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
